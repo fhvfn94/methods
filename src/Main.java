573326ad6e6a9e1a064ff1fe1e3e6290c883d48f
Эльдар К. Ишмуханбетов <IshmukhanbetovEK@mcms.local>
2022-10-27 21:56:07 +0300

    add all tasks

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,30 @@ public class Main {
             System.out.println(year + " — не високосный год");
         }
     }
+    public static void checkOS(int OS, int checkYearPhone) {
+        if (OS != 0 && checkYearPhone > 2014){
+            System.out.println("у тебя андроид все норм");
+            return;
+        } else if (OS == 0 && checkYearPhone > 2014) {
+            System.out.println("У тебя айфон все норм");
+        } else if (OS != 0 && checkYearPhone < 2015) {
+            System.out.println("У тебя андроид поставь легче версию");
+        } else if (OS == 0 && checkYearPhone < 2015) {
+            System.out.println("У тебя айфон поставь легче верию");
+        }
+    }
+    public static void delivery(int checkDelivery) {
+        if (checkDelivery < 20) {
+            System.out.println("Сутки");
+        } else if (checkDelivery >= 20 && checkDelivery < 60) {
+            System.out.println("сутки плюс день");
+        } else if (checkDelivery >= 60 && checkDelivery < 100) {
+            System.out.println("сутки плюс 2 дня");
+        }
+    }
     public static void main(String[] args) {
-        int a = 2023;
-        checkYear(a);
+        checkYear(2013);
+        checkOS(1, 2013);
+        delivery(95);
     }
 }
